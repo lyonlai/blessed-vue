1b994a6cc19853b2210fe1714dfa40565851dbad
Yun Lai <lyonlai1984@gmail.com>
2017-04-07 01:02:59 +1000

    Make a BlessedDom & BlessedPlaceboElement to accoomodate Vue's initial mount process.
    Now to do initial mount of your root element, you will need to
    
    1. const el = Vue.dom.createElement()
    2. new Vue({/* your configuration */}).$mount(el)
    
    This also solve the problem custom child component gone missing after mount. It's because the mounting strategy was wrong.

--- util/dom.js
+++ util/dom.js
@@ -1,22 +1,42 @@
 /* @flow */
-/* Fake dom */
-const doc = {
-  children: [],
+/*
+  Reason why this is here: There's no window.document equivalent in blessed,
+  This is here to accomondaet the mount on element situation.
+
+  Both the DOM and PlaceboELement are only here to simulate the behavior of web
+  dom.
+*/
+export class BlessedDOM {
+  constructor () {
+    this.children = []
+    this._events = {}
+  }
+
   append (c) {
+    if (c.program) {
+      this.screen = c
+    }
+    c.parent = this
     this.children.push(c)
-  },
+  }
+
   remove (c) {
     const index = this.children.indexOf(c)
     this.children.splice(index, 1)
   }
+
+  createElement () {
+    return new BlessedPlaceboElement(this)
+  }
 }
 
-const child = {
-  parent: doc
+export class BlessedPlaceboElement {
+  constructor (parent) {
+    this.parent = parent
+    this.elm = this
+  }
+
+  destroy () {}
 }
 
-child.elm = child
-
-doc.children.push(child)
-
-export const dom = doc
+export const dom = new BlessedDOM()
