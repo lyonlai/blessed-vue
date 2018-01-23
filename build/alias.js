a32a1fcea5771bc876441c13db1c6148e8aa37a3
Ian Reinhart Geiser <geiseri@geekcentral.pub>
2018-01-23 16:32:53 -0500

    Update to latest VueJS and a fix for issue #7.
    1. Update to latest VueJS.
    2. Update to latest rollup.
    3. Update to latest blessed-vue.
    4. Fix issue with missing weex dependency.
    5. Removed bundleSize because it was causing build errors.
    6. Update version to 2.0.0 so that Vuex will load correctly.
    
    All tests pass.

--- build/alias.js
+++ build/alias.js
@@ -7,6 +7,7 @@ module.exports = {
   core: path.resolve(__dirname, vuePath, 'src/core'),
   shared: path.resolve(__dirname, vuePath, 'src/shared'),
   web: path.resolve(__dirname, vuePath, 'src/platforms/web'),
+  weex: path.resolve(__dirname, vuePath, 'src/platforms/weex'),
   sfc: path.resolve(__dirname, vuePath, 'src/sfc'),
   runtime: path.resolve(__dirname, '../src/runtime'),
   util: path.resolve(__dirname, '../src/util')
--- build/config.js
+++ build/config.js
@@ -3,13 +3,16 @@ import buble from 'rollup-plugin-buble'
 import flow from 'rollup-plugin-flow-no-whitespace'
 import alias from 'rollup-plugin-alias'
 import replace from 'rollup-plugin-replace'
-import bundleSize from 'rollup-plugin-bundle-size'
+
 const version = process.env.VERSION || require('../package.json').version
 
 export default {
-  entry: path.resolve(__dirname, '../src/index.js'),
-  dest: path.resolve(__dirname, '../dist/build.js'),
-  format: 'cjs',
+  input: path.resolve(__dirname, '../src/index.js'),
+  output: {
+      name: 'BlessedVue',
+      file: path.resolve(__dirname, '../dist/build.js'),
+      format: 'cjs'
+  },
   plugins: [
     replace({
       __WEEX__: false,
@@ -17,8 +20,7 @@ export default {
     }),
     flow(),
     buble(),
-    alias(Object.assign({ he: './entity-decoder' }, require('./alias'))),
-    bundleSize()
+    alias(Object.assign({ he: './entity-decoder' }, require('./alias')))
   ],
   external: [
     'blessed',
